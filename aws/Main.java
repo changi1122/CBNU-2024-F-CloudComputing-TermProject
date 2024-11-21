4b5c08c636f85432b5c7ea90b03c3c79ec4dbf28
Woochang Lee <changi112242@outlook.com>
2024-11-21 14:04:20 +0900

    feat: Java 컴파일 및 실행 스크립트 추가

---
+++ aws/Main.java
@@ -0,0 +1,14 @@
+/*
+*  2024년 클라우드 컴퓨팅 Term Project
+*  작성자: 2019038074 이우창
+*  작성일: 2024-11-21 ~
+*/
+
+package aws;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Baseline");
+    }
+}
---
+++ run.ps1
@@ -0,0 +1,3 @@
+# Windows용 프로젝트 실행 스크립트 (PowerShell)
+
+java -cp ./build aws.Main
