f307e4ac50a721b68fbcc5f487bd691037b99352
Woochang Lee <changi112242@outlook.com>
2024-11-28 21:09:41 +0900

    feat:EC2 인스턴스에 셸 명령어 실행 기능 추가

--- aws/Main.java
+++ aws/Main.java
@@ -6,8 +6,7 @@
 
 package aws;
 
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
@@ -16,10 +15,16 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.*;
-import com.amazonaws.services.opsworks.model.StartInstanceRequest;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
+import com.amazonaws.services.simplesystemsmanagement.model.GetCommandInvocationRequest;
+import com.amazonaws.services.simplesystemsmanagement.model.GetCommandInvocationResult;
+import com.amazonaws.services.simplesystemsmanagement.model.SendCommandRequest;
+import com.amazonaws.services.simplesystemsmanagement.model.SendCommandResult;
 
 public class Main {
 
+    final static Regions region = Regions.US_EAST_1;
     static AmazonEC2 ec2;
 
     private static void init() throws Exception {
@@ -36,7 +41,7 @@ public class Main {
         }
         ec2 = AmazonEC2ClientBuilder.standard()
                 .withCredentials(credentialsProvider)
-                .withRegion(Regions.US_EAST_1)
+                .withRegion(region)
                 .build();
     }
 
@@ -55,11 +60,12 @@ public class Main {
             System.out.println("------------------------------------------------------------");
             System.out.println("           Amazon AWS Control Panel using SDK               ");
             System.out.println("------------------------------------------------------------");
-            System.out.println("  1. list instance                2. available zones        ");
-            System.out.println("  3. start instance               4. available regions      ");
-            System.out.println("  5. stop instance                6. create instance        ");
-            System.out.println("  7. reboot instance              8. list images            ");
-            System.out.println("                                 99. quit                   ");
+            System.out.println("  1.  list instance               2.  available zones       ");
+            System.out.println("  3.  start instance              4.  available regions     ");
+            System.out.println("  5.  stop instance               6.  create instance       ");
+            System.out.println("  7.  reboot instance             8.  list images           ");
+            System.out.println("  9.  shell command               10. _                     ");
+            System.out.println("                                  99. quit                  ");
             System.out.println("------------------------------------------------------------");
 
             System.out.print("Enter an integer: ");
@@ -125,6 +131,15 @@ public class Main {
 
                 case 8:
                     listImages();
+                    break;
+
+                case 9:
+                    System.out.print("Enter instance id: ");
+                    if (idString.hasNext())
+                        instanceId = idString.nextLine();
+
+                    if (!instanceId.trim().isEmpty())
+                        runShellCommand(instanceId.trim());
                     break;
 
                 case 99:
@@ -305,5 +320,88 @@ public class Main {
             System.out.printf("[ImageID] %s, [Name] %s, [Owner] %s\n",
                     images.getImageId(), images.getName(), images.getOwnerId());
         }
+    }
+
+    // EC2 인스턴스에서 셸 명령어를 실행하기 위한 메서드
+    public static void runShellCommand(String instanceId) throws InterruptedException {
+        AWSSimpleSystemsManagement ssmClient = AWSSimpleSystemsManagementClientBuilder.standard()
+                .withCredentials(new ProfileCredentialsProvider())
+                .withRegion(region)
+                .build();
+
+        String command;
+        Scanner input = new Scanner(System.in);
+
+        String workingDirectory = "/home/ec2-user";
+
+        while (true) {
+            System.out.printf("[ec2-user@%s %s]$ ", instanceId, displayWorkingDirectory(workingDirectory));
+            command = input.nextLine();
+            if (command.trim().isEmpty() || command.trim().equals("exit"))
+                break;
+
+            if (command.startsWith("cd ")) {
+                // change directory 로컬로 처리
+                workingDirectory = changeDirectory(workingDirectory, command.substring(3).trim());
+                continue;
+            }
+
+            SendCommandRequest sendCommandRequest = new SendCommandRequest()
+                    .withInstanceIds(instanceId)
+                    .withDocumentName("AWS-RunShellScript")
+                    .withParameters(Collections.singletonMap(
+                            "commands",
+                            List.of("cd "+workingDirectory, command))
+                    );
+
+            SendCommandResult result = ssmClient.sendCommand(sendCommandRequest);
+            String commandId = result.getCommand().getCommandId();
+
+            GetCommandInvocationRequest invocationRequest = new GetCommandInvocationRequest()
+                    .withInstanceId(instanceId)
+                    .withCommandId(commandId);
+
+            GetCommandInvocationResult invocationResult = null;
+            while (true) {
+                invocationResult = ssmClient.getCommandInvocation(invocationRequest);
+                String status = invocationResult.getStatus();
+
+                if ("Success".equals(status) || "Failed".equals(status) || "Cancelled".equals(status)) {
+                    break;
+                }
+                Thread.sleep(2000);
+            }
+
+            // System.out.println("[Command Status]: " + invocationResult.getStatus());
+            System.out.println(invocationResult.getStandardOutputContent());
+
+            String serror = invocationResult.getStandardErrorContent();
+            if (!serror.isEmpty())
+                System.out.println("\n[Standard Error]:\n" + serror);
+        }
+    }
+
+    // 로컬에서 working directory를 변경하기 처리하기 위한 메서드
+    private static String changeDirectory(String workingDirectory, String target) {
+        if (target.equals("~") || target.isEmpty()) { // 홈 디렉터리
+            return "/home/ec2-user";
+        }
+        else if (target.equals("..") && !workingDirectory.equals("/")) { // 상위 디렉터리
+            String wd = workingDirectory.substring(0, workingDirectory.lastIndexOf('/'));
+            if (wd.isEmpty())
+                wd = "/";
+            return wd;
+        }
+        else if (target.startsWith("/")) { // 절대 경로
+            return target;
+        }
+        else { // 상대 경로
+            return workingDirectory + "/" + target;
+        }
+    }
+    
+    // 로컬에서 working directory 출력시 홈 디렉터리를 ~로 출력하기 위한 메서드
+    private static String displayWorkingDirectory(String workingDirectory) {
+        return (workingDirectory.equals("/home/ec2-user") ? "~" : workingDirectory);
     }
 }
